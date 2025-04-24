9a5cebd31e56ea89b72c2e89f71cf9a7c9de7c65
Twig <littleb88888888@gmail.com>
2025-04-24 09:32:58 -0600

    Expanded arguments - corrected `api_key` to `api_token` - added address and debug arguments - added default values and help descriptions to arguments

--- arguments.py
+++ arguments.py
@@ -1,16 +1,29 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser(
-    prog="IDUN Guardian Jaw Clench Input Provider",
-    description="A simple app to pipe jaw clench predictions" \
-        "from the IDUN Guardian to binary input values"
+    prog='IDUN Guardian Jaw Clench Input Provider',
+    description='A simple app to pipe jaw clench predictions' \
+        'from the IDUN Guardian to binary input values'
 )
 
-parser.add_argument('-k', '--api-key')
+parser.add_argument(
+    '-t', '--api-token', default=None,
+    help='API Token required for IDUN API Calls'
+)
+parser.add_argument(
+    '-a', '--address', default=None,
+    help='MAC address of the guardian earbuds'
+)
+parser.add_argument(
+    '-d', '--debug', action='store_true', 
+    help='Enable debug mode'
+)
 
 
 class CommandLineArguments:
-    api_key: str
+    api_token: str | None
+    address: str | None
+    debug: bool
 
 
 def get_command_line_arguments() -> CommandLineArguments:
