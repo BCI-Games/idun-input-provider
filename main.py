2832e24fc2e1aa68f50e598aea287257beb6d20e
Twig <littleb88888888@gmail.com>
2025-04-24 10:05:08 -0600

    Fleshed out main script - sketched method to output predictions - applied command line arguments to client initialization - added calls to subscribe to predictions and start a recording

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import asyncio
 from idun_guardian_sdk import GuardianClient
+from warnings import warn
 from arguments import get_command_line_arguments
+from classes import PredictionMessage, PredictionType
+
+
+def output_jaw_clench_prediction(data):
+    prediction = PredictionMessage(data)
+    if prediction.type != PredictionType.JAW_CLENCH:
+        warn('Unexpected prediction type received: ' + prediction.type)
+    else:
+        print('Prediction received: ' + prediction.result)
+
 
 args = get_command_line_arguments()
+client = GuardianClient(
+    api_token=args.api_token,
+    address=args.address,
+    debug=args.address
+)
 
-client = GuardianClient(api_token=args.api_key)
+client.subscribe_realtime_predictions(jaw_clench=True, handler=output_jaw_clench_prediction)
+asyncio.run(client.start_recording())
