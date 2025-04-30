7afa228cf9c25d9e9d0cf6ef323b25dd039274c0
Twig <littleb88888888@gmail.com>
2025-04-30 14:56:15 -0600

    Changed output format - updated simulated input to send true/false strings instead of numbers

--- input_simulation.py
+++ input_simulation.py
@@ -7,5 +7,5 @@ async def start_simulated_input_broadcast_loop(
     server: BroadcastServer, max_delay: float = 2
 ):
     while True:
-        await server.send(str(getrandbits(1)))
+        await server.send(str(bool(getrandbits(1))))
         await sleep(random() * max_delay)
